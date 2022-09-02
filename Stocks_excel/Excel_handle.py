00d9902f97210eef651d3614d744d9d954da2cb8
SlaterBlack <sjblack139@gmail.com>
2022-09-03 07:14:18 +1000

    updates

--- Stocks_excel/Excel_handle.py
+++ Stocks_excel/Excel_handle.py
@@ -3,11 +3,11 @@ import openpyxl
 wb = openpyxl.load_workbook('stocks.xlsx')
 ws = wb['IBM']
 tData = []
-for row in ws.iter_rows(min_cols=1, max_col=4):
+for row in ws.iter_rows(min_col=1, max_col=4):
     date = row[0].value
     price=row[3].value
     if (date.year ==2010):
-        tData.append(price)
+        tData.append(price) 
 
 print("2010 Min ", min(tData))
 print("2010 Max ", max(tData))
