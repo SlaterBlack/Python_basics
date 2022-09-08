f5a33c7fa589c44dbdb89186e26933baf0e00245
SlaterBlack <sjblack139@gmail.com>
2022-09-08 13:24:41 +1000

    g

--- GUIs/basicGUI.py
+++ GUIs/basicGUI.py
@@ -9,12 +9,26 @@ class myGui (wx.Frame):
     def initialise(self):
         pnl = wx.Panel(self)
         #   Adding text 
-        self.st = wx.StaticText(pnl,label = "Hello World")      #   insert static text
-        fnt = self.st.GetFont()                                 #   Obtain font of text stored in "st" and store it in order to change it
+        self.s1 = wx.StaticText(pnl,label = "Hello World")      #   insert static text
+        self.s2 = wx.StaticText(pnl,label = "Hello World")      #   insert static text
+        self.s3 = wx.StaticText(pnl,label = "Hello World")      #   insert static text
+        self.s4 = wx.StaticText(pnl,label = "Hello World")      #   insert static text
+        self.s5 = wx.StaticText(pnl,label = "Hello World")      #   insert static text
+        self.s6 = wx.StaticText(pnl,label = "Hello World")      #   insert static text
+
+        self.s1.SetPosition((10, 10))
+        self.s2.SetPosition((10, 30))
+        self.s3.SetPosition((10, 50))
+        self.s4.SetPosition((10, 70))
+        self.s5.SetPosition((10, 90))
+        self.s6.SetPosition((10, 110))
+
+
+        fnt = self.s1.GetFont()                                 #   Obtain font of text stored in "st" and store it in order to change it
         fnt.PointSize +=10                                      #   Change font size
         fnt = fnt.Bold()                                        #   Make font bold
-        self.st.SetFont(fnt)                                    #   Set font setting (activate previous changes)
-        self.st.SetLabel("Some new text")
+        self.s1.SetFont(fnt)                                    #   Set font setting (activate previous changes)
+        self.s1.SetLabel("Some new text")
 
         #   frame customising
         self.Center()                                           #   Place frame in centre of screen
@@ -23,7 +37,7 @@ class myGui (wx.Frame):
 
         
         #   Adding a button
-        btn = wx.Button(pnl,pos=(25,100),label="Don't press this button")
+        btn = wx.Button(pnl,pos=(25,200),label="Don't press this button")
         self.Bind(wx.EVT_BUTTON,self.onButton,btn)
 
         #   Adding a menu
