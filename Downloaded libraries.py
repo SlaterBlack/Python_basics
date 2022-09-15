d12171bdc2665285345feb2444fcfc5982590136
SlaterBlack <sjblack139@gmail.com>
2022-09-15 10:00:45 +1000

    adds

---
+++ Downloaded libraries.py
@@ -0,0 +1,9 @@
+import wx #GUI 
+import wx.grid as grid
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.figure as figure
+import sqlite3
+import openpyxl
+import pandas
+import re
